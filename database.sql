af869087ccdc5e38c508c36bd2853473f568b1ad
Tia Theisen <tiatheisen@gmail.com>
2022-10-07 16:52:24 -0500

    updated database table to include status and date created

--- database.sql
+++ database.sql
@@ -1,4 +1,6 @@
 CREATE TABLE "tasks" (
   "id" serial primary key,
   "task" VARCHAR (500) NOT NULL,
+  "status" BOOLEAN DEFAULT false,
+  "created at" DATE DEFAULT NOW()
 );
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -11,9 +11,12 @@ $( document ).ready( function(){
 
   function addClickListeners(){
     console.log('in addClickListeners');
-
         $('#addBtn').on('click', newTask);
-    
+        $('#taskTable').on('click', '#completeBtn', markComplete);
+        $('#taskTable').on('click', '#deleteBtn', deleteTask);
+        
+
+
   }
 
   //Creating a function to load the tasks on the page
@@ -59,9 +62,36 @@ $( document ).ready( function(){
             })
   }
 
-  function renderTasks(){
+  function deleteTask(){
+        console.log('in DeleteTask');
+  }
+
+
+
+  function markComplete(){
+        console.log('in markComplete');
+  }
+
+
+  function renderTasks(tasks){
         console.log('in render');
+        //emptying table
+        $('#taskTable').empty();
+        //Appending first row of table with titles (so it is always there)
+        $('#taskTable').append(`
+            <tr>
+                <td>Task</td>
+                <td>Status</td>
+            </tr>
 
-
-
+        `);
+    for(let task of tasks){
+        $('#taskTable').append(`
+            <tr>
+            <td>${task.task}</td>
+            <td><button class="completeBtn" data-id="${task.id}">Complete</button></td>
+            <td><button class="deleteBtn" data-id="${task.id}">Delete</button></td>
+            </tr>
+        `)
+  }
   }
