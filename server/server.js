99ec3531f868ab6fc08508f038ad62b6a395b124
Tia Theisen <tiatheisen@gmail.com>
2022-10-07 12:35:35 -0500

    add express, body-parser, and port to server.js file

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// Require express
+const express = require('express');
+
+// Create an instance of express by calling the function returned above - gives us an object
+const app = express();
+const port = 5000;
+
+// express static file serving - public is the folder name
+app.use(express.static('server/public'));
+
+//adding in body-parser
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Start up our server
+app.listen(port, () => {
+  console.log('listening on port', port);
+});
